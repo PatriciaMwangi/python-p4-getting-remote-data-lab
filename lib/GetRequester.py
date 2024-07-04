b0db6ce7e233793b2aa5ed6a722c32e39c0f0844
Patricia Mwangi <patricia.mwangi@student.moringaschool.com>
2024-07-04 16:55:40 +0300

    getting remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,22 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response=requests.get(self.url)
+        return response.content
+        
 
-    def load_json(self):
-        pass
+    def load_json(self,response):
+        try:
+            jsons=json.loads(response)
+            return jsons
+        except json.JSONDecodeError as e:
+            print(f'Failed to decode: {e}')
+            return None
+    
+cls=GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+get=cls.get_response_body()
+getting=cls.load_json(get)
+
+#print(get)
+#print(getting[1])
+        
